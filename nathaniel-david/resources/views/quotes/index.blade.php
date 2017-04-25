cc0486f7265c7def6fcbc7c0bad8e402b1212fdf
Nathaniel-David <me@nathaniel-david.tech>
2017-04-25 14:33:16 -0500

    new edit

--- nathaniel-david/resources/views/quotes/index.blade.php
+++ nathaniel-david/resources/views/quotes/index.blade.php
@@ -4,7 +4,15 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <h1>Quotes</h1>
+            <h1>Quotes</h1><hr>
+            @foreach ($objects as $quote)
+             <p>Name: {{ $quote->name }}</p>
+             <p>Company: {{ $quote->company }}</p>
+             <p>Email: {{ $quote->email }}</p>
+             <p>Phone: {{ $quote->phone }}</p>
+             <p>About: {{ $quote->about }}</p>
+             <hr>
+            @endforeach
         </div>
     </div>
 </div>
