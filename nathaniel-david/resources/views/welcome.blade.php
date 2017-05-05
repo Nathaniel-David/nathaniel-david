6768c06f3704fe4a6be1ea143ea7867a201ce80d
Nathaniel-David <me@nathaniel-david.tech>
2017-05-05 13:52:34 -0500

    meta tag

--- nathaniel-david/resources/views/welcome.blade.php
+++ nathaniel-david/resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-site-verification" content="0gr93PphLUUt2NRg-EFAdzXreM-jt7qkNfbASCh9_I0" />
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>Austin Web Development - NATHANIEL-DAVID</title>
 
