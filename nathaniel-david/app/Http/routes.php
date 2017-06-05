44f4b1871a0e9271fd1b41e4cfcd7bf13b15f29f
Nathaniel-David <me@nathaniel-david.tech>
2017-06-05 17:00:19 -0500

    protected routes

--- nathaniel-david/app/Http/routes.php
+++ nathaniel-david/app/Http/routes.php
@@ -23,9 +23,13 @@ Route::get('process', function () {
     return view('process');
 });
 
+Route::get('/quotes', [
+    'middleware' => 'auth',
+    'uses' => 'QuotesController@index'
+]);
+
 Route::get('thankyou', 'QuotesController@thankyou');
 Route::resource('quote', 'QuotesController');
-Route::get('/quotes', 'QuotesController@index');
 Route::get('quote', 'QuotesController@create');
 Route::post('quote', 'QuotesController@store');
 
--- nathaniel-david/resources/views/welcome.blade.php
+++ nathaniel-david/resources/views/welcome.blade.php
@@ -39,6 +39,19 @@
          
              <div class="navbar-collapse collapse nav-menu">
                  <ul class="nav navbar-nav navbar-right">
+                    @if (Auth::guest())
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="{{ url('/quotes') }}"></i>Quotes</a></li>
+                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                            </ul>
+                        </li>
+                    @endif
                    <li class=""><a href="/services">Services</a></li>
                    <li> <a href="/process">Process</a> </li>
                    <li> <a href="/quote" class="btn btn-primary btn-danger">Get A Quote</a> </li>
